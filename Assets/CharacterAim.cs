09041d0c9a2c724904dd0f6faa6bb82b7659ef6a
Jesper Møller Jensen <jesp626a@edu.eal.dk>
2018-10-05 21:15:20 +0200

    Added Scene and AimController Script

---
+++ Assets/CharacterAim.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    var Hori = Input.GetAxisRaw("AimHorizontal");
+	    var Vert = Input.GetAxisRaw("AimVertical");
+
+	    Debug.Log(string.Format("H = {0} : V = {1}", Hori, Vert));
+
+	    Vector2 dir = new Vector2(Hori, Vert);
+
+        Debug.DrawLine(transform.position,transform.position + new Vector3(dir.x,dir.y,0)*5);
+
+
+	}
+}
