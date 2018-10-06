0bc21850fbfe248127061e4278104956a7c05044
Jesper <jesp626a@edu.eal.dk>
2018-10-06 19:14:33 +0200

    ADded Zombie behavior

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -1,74 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(CircleCollider2D))]
 public class ZombieMovement : MonoBehaviour
 {
 
     private Rigidbody2D _rigidbody;
-    private CircleCollider2D _vision;
     private GameObject _target;
 
     [SerializeField] private float Speed = 3f;
-    [SerializeField] private float JumpSpeed = 3f;
     [SerializeField] private float VisionRange = 10f;
 
-    private bool isOnGround = true;
 
     // Use this for initialization
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _vision = GetComponent<CircleCollider2D>();
-        _vision.radius = VisionRange;
+        InvokeRepeating("FindPlayer", 0.0f, Random.Range(0.4f, 0.5f));
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (_target != null)
-        {
-            MoveToPlayer();
-        }
-
-        var t1 = Physics2D.Raycast(_rigidbody.position - new Vector2(0.45f, 1.01f), new Vector2(0, -1), 0.01f);
-        var t2 = Physics2D.Raycast(_rigidbody.position - new Vector2(-0.45f, 1.01f), new Vector2(0, -1), 0.01f);
-
-        if (t1.transform != null || t2.transform != null)
-        {
-            isOnGround = true;
-        }
-        else
-        {
-            isOnGround = false;
-        }
-
-        if (Input.GetButtonDown("Jump") && isOnGround)
-        {
-            Jump();
-        }
     }
 
     void MoveToPlayer()
     {
-        var horizontal = _target.transform.position.x - transform.position.x;
+        var horizontal = Mathf.Sign(_target.transform.position.x - transform.position.x);
         _rigidbody.velocity = new Vector2(horizontal * Speed, _rigidbody.velocity.y);
     }
 
-    void Jump()
+    void StopMoving()
     {
-        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, JumpSpeed);
+        _rigidbody.velocity = new Vector2(0,_rigidbody.velocity.y);
     }
 
-    void OnTriggerEnter2D(Collider2D collider)
+    void FindPlayer()
     {
-        if (collider.gameObject.CompareTag("Player"))
+        if (_target == null)
         {
-            _target = collider.gameObject;
+            _target = GameObject.FindGameObjectWithTag("Player");
+            Debug.Log("Player found");
+        }
+        else
+        {
+            int layerMask = 1 << 8;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position,
+                VisionRange,LayerMask.GetMask("Player")+LayerMask.GetMask("Default"));
+            Debug.DrawRay(transform.position, _target.transform.position - transform.position);
+            Debug.Log("Trying to hit player with ray");
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    _target = hit.collider.gameObject;
+                    MoveToPlayer();
+                    Debug.Log("Move to player");
+                }
+                else
+                {
+                    StopMoving();
+                }
+            }
+
         }
     }
+
+
 }
