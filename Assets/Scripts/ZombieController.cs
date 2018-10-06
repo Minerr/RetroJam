3179d7cf1cf645cd88704c70be829a58c52ef0f0
Jesper <jesp626a@edu.eal.dk>
2018-10-06 20:52:08 +0200

    Ai and more weapons

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class ZombieController : MonoBehaviour {
+
+    [SerializeField] private float MAX_HEALTH = 100f;
+    [SerializeField] private float DAMAMGE = 10f;
+    [SerializeField] private float ATTACK_COOLDOWN = 1f;
+    [SerializeField] private float ATTACK_RANGE = 1f;
+
+    public ZombieMovement ZombieMovement;
+
+    [SerializeField] private float VisionRange = 10f;
+
+    private float _currentHealth;
+    private GameObject _target;
+    private bool _canAttack = true;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("FindPlayer", 0.0f, Random.Range(0.4f, 0.5f));
+        _currentHealth = MAX_HEALTH;
+    }
+	
+
+    public void TakeDamage(float amount)
+    {
+        Debug.Log(amount);
+        _currentHealth -= amount;
+
+        if (_currentHealth <= 0)
+        {
+            _currentHealth = 0;
+            if (_currentHealth <= 0)
+            {
+                Death();
+            }
+        }
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(ATTACK_COOLDOWN);
+        _canAttack = true;
+    }
+
+    void Attack()
+    {
+        _target.gameObject.GetComponent<PlayerController>().TakeDamage(DAMAMGE);
+    }
+
+    void FindPlayer()
+    {
+        if (_target == null)
+        {
+            _target = GameObject.FindGameObjectWithTag("Player");
+        }
+        else
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position,
+                VisionRange, LayerMask.GetMask("Player") + LayerMask.GetMask("Default"));
+            Debug.DrawRay(transform.position, _target.transform.position - transform.position);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.CompareTag("Player"))
+                {
+                    _target = hit.collider.gameObject;
+                    ZombieMovement.MoveToPlayer(_target);
+                    if (hit.distance < ATTACK_RANGE && _canAttack)
+                    {
+                        _canAttack = false;
+                        Attack();
+                        StartCoroutine(AttackCooldown());
+                    }
+                }
+                else
+                {
+                    ZombieMovement.StopMoving();
+                }
+            }
+
+        }
+    }
+}
--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -10,66 +10,25 @@ public class ZombieMovement : MonoBehaviour
 {
 
     private Rigidbody2D _rigidbody;
-    private GameObject _target;
 
     [SerializeField] private float Speed = 3f;
-    [SerializeField] private float VisionRange = 10f;
 
 
     // Use this for initialization
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        InvokeRepeating("FindPlayer", 0.0f, Random.Range(0.4f, 0.5f));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveToPlayer(GameObject target)
     {
-    }
-
-    void MoveToPlayer()
-    {
-        var horizontal = Mathf.Sign(_target.transform.position.x - transform.position.x);
+        var horizontal = Mathf.Sign(target.transform.position.x - transform.position.x);
         _rigidbody.velocity = new Vector2(horizontal * Speed, _rigidbody.velocity.y);
     }
 
-    void StopMoving()
+    public void StopMoving()
     {
         _rigidbody.velocity = new Vector2(0,_rigidbody.velocity.y);
     }
-
-    void FindPlayer()
-    {
-        if (_target == null)
-        {
-            _target = GameObject.FindGameObjectWithTag("Player");
-            Debug.Log("Player found");
-        }
-        else
-        {
-            int layerMask = 1 << 8;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position,
-                VisionRange,LayerMask.GetMask("Player")+LayerMask.GetMask("Default"));
-            Debug.DrawRay(transform.position, _target.transform.position - transform.position);
-            Debug.Log("Trying to hit player with ray");
-
-            if (hit.collider != null)
-            {
-                if (hit.collider.gameObject.CompareTag("Player"))
-                {
-                    _target = hit.collider.gameObject;
-                    MoveToPlayer();
-                    Debug.Log("Move to player");
-                }
-                else
-                {
-                    StopMoving();
-                }
-            }
-
-        }
-    }
-
 
 }
